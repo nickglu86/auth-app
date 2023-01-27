18dfbb18f85b44632103a83052c79c6f3c954024
nickglu86 <enayfls@gmail.com>
2023-01-27 13:06:19 +0200

    pretify

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,27 +1,22 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 
 async function dbConnect() {
-    // mongoose.set("strictQuery", false);
-    mongoose
-    .connect(
-        process.env.DB_URL,
-      {
-        //   these are options to ensure that the connection is done properly
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    )
+  // mongoose.set("strictQuery", false);
+  mongoose
+    .connect(process.env.DB_URL, {
+      //   these are options to ensure that the connection is done properly
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
     .then(() => {
-        console.log("Successfully connected to MongoDB Atlas!");
-      })
-      .catch((error) => {
-        console.log("Unable to connect to MongoDB Atlas!");
-        console.error(error);
-      });
-  
-
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
 }
 
 module.exports = dbConnect;
--- db/userModel.js
+++ db/userModel.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, "Please provide an Email!"],
-        unique: [true, "Email Exist"],
-      },
-    
-      password: {
-        type: String,
-        required: [true, "Please provide a password!"],
-        unique: false,
-      },
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
 });
 
 //create a user table or collection if there is no table with that name already
